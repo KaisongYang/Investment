caec5729a42576a93ea9ae3875f0ddc4e4700de8
yangkaisong <yangkaissong@163.com>
2018-02-27 21:10:03 +0800

    创建项目

---
+++ Investment/InvestmentTests/InvestmentTests.m
@@ -0,0 +1,39 @@
+//
+//  InvestmentTests.m
+//  InvestmentTests
+//
+//  Created by kk on 2018/2/27.
+//  Copyright © 2018年 KaisongYang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InvestmentTests : XCTestCase
+
+@end
+
+@implementation InvestmentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
